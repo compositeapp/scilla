f5a792cb8a211bda04dbc47b2491120d661cf458
Anton Trunov <anton@zilliqa.com>
2019-02-28 15:37:15 +0300

    Reduce boilerplate for ppx_expect tests

--- tests/ppx-expect-tests/TestSyntax.ml
+++ tests/ppx-expect-tests/TestSyntax.ml
@@ -1,13 +1,20 @@
 open Base
 open Syntax
 
+let print actual = actual |> Sexp.to_string_hum |> Stdio.print_endline
+
 let%expect_test _ =
   let actual = free_tvars (FunType ((TypeVar "'X"), (TypeVar "'Y"))) in
-  [%sexp_of: string list] actual |> Sexp.to_string_hum |> Stdio.print_endline;
+  [%sexp_of: string list] actual |> print;
+  [%expect {| ('Y 'X) |}]
+
+let%expect_test _ =
+  let actual = free_tvars (FunType ((TypeVar "'X"), (TypeVar "'Y"))) in
+  [%sexp_of: string list] actual |> print;
   [%expect {| ('Y 'X) |}]
 
 let%expect_test _ =
    let actual = subst_types_in_type ["'X", TypeVar "'Y"; "'Y", TypeVar "'X"] (TypeVar "'X") in
-   [%sexp_of: typ] actual |> Sexp.to_string_hum |> Stdio.print_endline;
+   [%sexp_of: typ] actual |> print;
    [%expect {| (TypeVar 'X) |}]
 
